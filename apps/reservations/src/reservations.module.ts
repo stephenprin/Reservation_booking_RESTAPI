e0ffb50f297b3b913a3858e304c19eb9354aa746
Prince Nmezi <stephenprince427@gmail.com>
2024-01-04 00:48:31 +0100

    configuration and file structuring

---
+++ apps/reservations/src/reservations.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ReservationsService } from './reservations.service';
+import { ReservationsController } from './reservations.controller';
+import { DatabaseModule } from '@app/common';
+import { ReservationRepository } from './reservation.repository';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './model/reservation.schema';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: ReservationDocument.name, schema: ReservationSchema },
+    ]),
+  ],
+  controllers: [ReservationsController],
+  providers: [ReservationsService, ReservationRepository],
+})
+export class ReservationsModule {}
---
+++ apps/reservations/src/reservations.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { CreateReservationDto } from './dto/create-reservation.dto';
+import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { ReservationRepository } from './reservation.repository';
+
+@Injectable()
+export class ReservationsService {
+  constructor(private reservationRepository: ReservationRepository) {}
+  create(createReservationDto: CreateReservationDto) {
+    return this.reservationRepository.create(createReservationDto)
+  }
+
+  findAll() {
+    return `This action returns all reservations`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} reservation`;
+  }
+
+  update(id: number, updateReservationDto: UpdateReservationDto) {
+    return `This action updates a #${id} reservation`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} reservation`;
+  }
+}
