86d2755119fad3dbf5920bc7677d37d0e5bba825
Prince Nmezi <stephenprince427@gmail.com>
2024-01-04 17:07:51 +0100

    dockerize reservations

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
